d7e68cfefb144412b5bd948afa7db53ff9a04e8c
aurelie_dumotier <aurelie.dumotier@gmail.com>
2025-10-29 12:05:39 +0100

    finish serve-employees Back

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -12,6 +13,8 @@ const sampleEmployee = {
         medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
     },
 };
+
+app.use(cors({ origin: ["http://localhost:5173"]}));
 
 app.get("/api/employees", (req, res) => {
     res.json({results : [sampleEmployee] });
